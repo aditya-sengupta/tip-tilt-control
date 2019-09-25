5f877a184e1ddedbf65567a75f999fe23fbc650d
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2019-09-25 11:00:07 -0700

    things that were working aren't

--- control/dev/aberrations.py
+++ control/dev/aberrations.py
@@ -6,7 +6,7 @@ sys.path.append("..")
 from hcipy.hcipy import *
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import signal, fftpack
+from scipy import signal, ndimage
 from copy import deepcopy
 
 N_vib_app = 10
@@ -20,17 +20,17 @@ times = np.arange(0, time_id, 1 / f_sampling)  # array of times to operate on
 num_steps = int(time_id * f_sampling)
 D = 10.95
 r0 = 16.5e-2   
-pupil_size = 40
-coeff_deconv = 20 # power to raise the deconvolved image to, to create better contrast for the CM
-focal_samples = 20 # samples per lambda over D
-focal_width = 20 # the number of lambda over Ds
-s = focal_samples * pupil_size
+pupil_size = 16
+focal_samples = 8 # samples per lambda over D
+focal_width = 8 # half the number of lambda over Ds
+D_magic = 1
 wavelength = 500e-9 # meters
-pupil_grid = make_pupil_grid(pupil_size)
+focal_size = 2 * focal_samples * focal_width
+pupil_grid = make_pupil_grid(pupil_size, D_magic)
 focal_grid = make_focal_grid_from_pupil_grid(pupil_grid, focal_samples, focal_width, wavelength=wavelength)
-propagate = FraunhoferPropagator(pupil_grid, focal_grid)
-aperture = circular_aperture(1)(pupil_grid)
-psf = np.array(propagate(Wavefront(aperture, wavelength)).intensity.reshape((s, s)))
+prop = FraunhoferPropagator(pupil_grid, focal_grid)
+aperture = circular_aperture(D_magic)(pupil_grid)
+layers = make_standard_atmospheric_layers(pupil_grid)
 
 make_vib_amps = lambda N: np.random.uniform(low=0.1, high=1, size=N)  # milliarcseconds
 make_vib_freqs = lambda N:  np.random.uniform(low=f_1, high=f_2, size=N)  # Hz
@@ -75,57 +75,30 @@ def make_noisy_data(pos, noise=measurement_noise):
 
 def center_of_mass(f):
     # takes in a Field, returns its CM.
-    dims = np.array([np.max(f.grid.x) - np.min(f.grid.x), np.max(f.grid.y) - np.min(f.grid.y)])
-    average = np.array([(f.grid.x * f).sum(), (f.grid.y * f).sum()]) / (f.sum())
-    return average * focal_width * focal_samples / dims
+    s = f.grid.shape[0]
+    x, y = (n.flatten() for n in np.meshgrid(np.linspace(-s/2, s/2-1, s), np.linspace(-s/2, s/2-1, s)))
+    return np.round(np.array((sum(f*x), sum(f*y)))/sum(f), 3)
 
-
-def correction_for(self, wavelength):
-    return self.transformation_matrix.dot(self.corrected_coeffs[0] / wavelength)
-
-def correct_until(self, t):
-    self.layer.evolve_until(t)
-
-    if len(self.corrected_coeffs) > 0:
-        coeffs = self.transformation_matrix_inverse.dot(self.correction_for(1))
-    else:
-        coeffs = self.transformation_matrix_inverse.dot(self.layer.phase_for(1))
-    if len(self.corrected_coeffs) > self.lag:
-        self.corrected_coeffs.pop(0)
-    self.corrected_coeffs.append(coeffs)
-    
-ModalAdaptiveOpticsLayer.correction_for = correction_for
-ModalAdaptiveOpticsLayer.correct_until = correct_until
 
 def make_specific_tt(weights):
     # weights = number of desired lambda-over-Ds the center of the PSF is to be moved. Tuple with (tip_wt, tilt_wt).
-    tt = [zernike(*ansi_to_zernike(i), 1)(pupil_grid) for i in (1, 2)]
-    tt_wf = Wavefront(aperture * np.exp(1j * np.pi * sum([w * z for w, z in zip(tt_weights, tt)]) * scale), wavelength)
+    tt = [zernike(*ansi_to_zernike(i), D_magic)(pupil_grid) for i in (1, 2)]
+    scale = focal_samples / 4.86754191 # magic number to normalize to around one lambda-over-D
+    tt_wf = Wavefront(aperture * np.exp(1j * sum([w * z for w, z in zip(tt_weights, tt)]) * scale), wavelength)
     return tt_wf
 
-# source: https://stackoverflow.com/questions/17473917/is-there-a-equivalent-of-scipy-signal-deconvolve-for-2d-arrays
-def deconvolve(spot, psf):
-    spot_array = np.array(spot.reshape((s, s)))
-    spot_fft = fftpack.fftshift(fftpack.fftn(spot_array))
-    psf_fft = fftpack.fftshift(fftpack.fftn(psf))
-    return Field(np.real(fftpack.fftshift(fftpack.ifftn(fftpack.ifftshift(spot_fft/psf_fft)))).flatten(), focal_grid)
 
-def make_atm_data(t=time_id):
-    layers = make_standard_atmospheric_layers(pupil_grid)
-    tt = [zernike(*ansi_to_zernike(i), 1)(pupil_grid) for i in (1, 2)] # tip-tilt phase basis
-    MOAlayers = [ModalAdaptiveOpticsLayer(layer, controlled_modes=ModeBasis(tt), lag=0) for layer in layers]
+def make_atm_data(wf=None):
     conversion = (wavelength / D) * 206265000 / focal_samples
+    if wf is None:
+        wf = Wavefront(aperture, wavelength) # can induce a specified TT here if desired
 
-    tt_cms = np.zeros((int(f_sampling * t), 2))
-    for n in range(int(f_sampling * t)):
-        wf = Wavefront(aperture, wavelength)
-        phase = wf.phase
-        for layer in MOAlayers:
-            layer.correct_until(n / f_sampling)
-            phase += layer.phase_for(wavelength)
-        wf = Wavefront(aperture * np.exp(1j * phase), wavelength)
-        total_intensity = propagate(wf).intensity
-        tt_cms[n] = center_of_mass(deconvolve(total_intensity, psf) ** coeff_deconv)
+    tt_cms = np.zeros((f_sampling * time_id, 2))
+    for n in range(f_sampling * time_id):
+        for layer in layers:
+            layer.evolve_until(times[n])
+            wf = layer(wf)
+        tt_cms[n] = center_of_mass(prop(wf).intensity)
 
     tt_cms *= conversion # pixels to mas
     return tt_cms
