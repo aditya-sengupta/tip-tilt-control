18d9d907640c6d800645cee345a86a3f4dd98815
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2019-10-30 21:33:09 -0700

    quickly before it gets worse

--- control/dev/controller.py
+++ control/dev/controller.py
@@ -8,63 +8,16 @@ from copy import deepcopy
 
 rms = lambda data: np.sqrt(np.mean(data ** 2))
 
-def control(truth, controller, noise=0.06, delay=1):
-    '''
-    Simulates the general control problem.
-
-    Parameters:
-
-    truth - ndarray
-    The uncorrected truth values of tip or tilt. Conventionally milliarcseconds: one per timestep.
-
-    controller - Control object
-    The controller: takes in the open-loop measurements, returns a sequence of control actions in the same units.
-
-    noise - float
-    The noise to add to truth to make open-loop measurements.
-
-    delay - int
-    The frame delay of the control setup being simulated, to be passed in as input to strategy. Should only be 1-3.
-
-    Returns:
-
-    residual - ndarray
-    The residual at each step.
-    '''
-    residuals = np.zeros(truth.size)
-    controller.delay = delay
-    time = controller.calibration_time
-    if time is None:
-        time = delay
-    calibration = truth[:time] + np.random.normal(0, noise, time)
-    controller.make_state(calibration)
-    residuals[:time] = calibration
-    actions = np.zeros(truth.size)
-
-    shifts = np.diff(truth)
-    # shifts[i] is the shift from state i to state i + 1
-    # i.e. position[i+1] = position[i] + shifts[i] + actions[i]
-    # where you were before, plus the change due to environment, plus the control action applied
-
-    for i in range(time, truth.size - 1):
-        residuals[i] = residuals[i - 1] + shifts[i - 1] + actions[i]
-        measurement = residuals[i] + np.random.normal(0, noise)
-        if i + delay < truth.size:
-            actions[i + delay] = -controller.strategy(measurement)
-    
-    return residuals, actions
-
-
 class Controller:
-    def __init__(self, method, *args, STATE_SIZE=None, calibration_time=None):
+    def __init__(self, method, *args, STATE_SIZE=0, calibration_time=None):
         if method == 'stdint':
             self.STATE_SIZE = 3
+            self.calibration_time = 3
         else:
             self.STATE_SIZE = STATE_SIZE
-        if calibration_time is None:
-            self.calibration_time = self.STATE_SIZE
+            self.calibration_time = calibration_time
         self.state = np.zeros(STATE_SIZE,)
-        self.delay = 0
+        self.delay = 1
         if method == 'baseline':
             self.strategy = self.strategy_baseline
             self.make_state = self.strategy_baseline
@@ -76,6 +29,53 @@ class Controller:
             self.strategy = self.strategy_kalman
             self.make_state = self.make_state_AR
             self.kfilter = args[0]
+            self.calibration_time = self.kfilter.state.size
+
+    def control(self, truth, noise=0.06):
+        '''
+        Simulates the general control problem.
+
+        Parameters:
+
+        truth - ndarray
+        The uncorrected truth values of tip or tilt. Conventionally milliarcseconds: one per timestep.
+
+        noise - float
+        The noise to add to truth to make open-loop measurements.
+
+        delay - int
+        The frame delay of the control setup being simulated, to be passed in as input to strategy. Should only be 1-3.
+
+        Returns:
+
+        residuals - ndarray
+        The residual at each step.
+
+        actions - ndarray
+        The control actions as a cumulative sum of individual control actions.
+        '''
+        residuals = np.zeros(truth.size)
+        time = self.calibration_time
+        if time is None:
+            time = self.delay
+        calibration = truth[:time] + np.random.normal(0, noise, time)
+        self.make_state(calibration)
+        residuals[:time] = truth[:time]
+        actions = np.zeros(truth.size)
+
+        shifts = np.diff(truth)
+        # shifts[i] is the shift from state i to state i + 1
+        # i.e. position[i+1] = position[i] + shifts[i] + actions[i]
+        # where you were before, plus the change due to environment, plus the control action applied
+
+        print(time)
+        for i in range(time, truth.size):
+            residuals[i] = residuals[i - 1] + shifts[i - 1] - actions[i - 1]
+            measurement = residuals[i] + np.random.normal(0, noise)
+            if i + self.delay < truth.size:
+                actions[i + self.delay] = self.strategy(measurement)
+        
+        return residuals, np.cumsum(actions)
 
     def make_state_AR(self, calibration):
         # for autoregressive strategies, makes an initial state that's the first N openloop measurements
@@ -95,38 +95,36 @@ class Controller:
 
     def strategy_kalman(self, measurement):
         # describes a 'naive Kalman' control scheme, i.e. not LQG
+        self.kfilter.state = self.state
+        assert self.kfilter.state.any(), "starting from zero state"
         self.kfilter.update(measurement)
         self.kfilter.predict()
-        state_pred = deepcopy(self.kfilter.state)
+        self.state = self.kfilter.state
+        state_pred = deepcopy(self.state)
         for _ in range(self.delay - 1):
             state_pred = self.kfilter.A.dot(state_pred)
-        return self.kfilter.measure(state_pred)
+        return -self.kfilter.measure(state_pred)
 
-def run_stdint(size):
-    stdint = Controller('stdint', 3)
-    truth = np.load('keck_tt/OpenLoop_n0088.npy')[:size,0]
-    residuals, actions = control(truth, stdint)
-    return truth, residuals, actions
+size = 1000
+keck_normalizer = 0.6 * (600e-9 / (2 * np.pi)) *  206265000
+truth = np.load('keck_tt/OpenLoop_n0088.npy')[:size,0] * keck_normalizer
+kalman = Controller('kalman', make_kfilter_turb(make_impulse(truth[:size//2], N=200)))
+stdint = Controller('stdint')
+baseline = Controller('baseline')
 
-def run_baseline(size):
-    baseline = Controller('baseline')
-    truth = np.load('keck_tt/OpenLoop_n0088.npy')[:size,0]
-    residuals, actions = control(truth, baseline)
-    return truth, residuals, actions
-
-def show_control(truth, residuals, actions):
+def show_control(controller_name):
+    controller = globals()[controller_name]
+    residuals, actions = controller.control(truth)
+    plt.figure(figsize=(10,10))
     plt.plot(truth, label='Truth')
     plt.plot(actions, label='Actions')
     plt.plot(residuals, label='Residual')
     plt.legend()
-    plt.title("Truth and actions and residuals, error = " + str(rms(residuals)))
+    plt.title("Control action from " + controller_name + ", error = " + str(rms(residuals)))
     plt.show()
 
 def keck_control(size):
-    openloop = np.load('keck_tt/OpenLoop_n0088.npy')[:size,0]
-    commands = np.load('keck_tt/Commands_n0088.npy')[:size,0]
-    centroids = np.load('keck_tt/Centroid_n0088.npy')[:size,0]
+    openloop = np.load('keck_tt/OpenLoop_n0088.npy')[:size,0] * keck_normalizer
+    commands = np.load('keck_tt/Commands_n0088.npy')[:size,0] * keck_normalizer
+    centroids = np.load('keck_tt/Centroid_n0088.npy')[:size,0] * keck_normalizer
     return openloop, centroids, commands
-
-if __name__ == "__main__":
-    show_control(*run_stdint(1000))
